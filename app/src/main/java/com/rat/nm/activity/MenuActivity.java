fb114283ace83815e45504a163ce44668a25fae4
laoshubuluo <541276229@qq.com>
2015-12-27 22:25:48 +0800

    项目基本框架搭建（部分）

--- app/src/main/java/com/rat/nm/activity/MenuActivity.java
+++ app/src/main/java/com/rat/nm/activity/MenuActivity.java
@@ -1,6 +1,5 @@
 package com.rat.nm.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
 import android.view.View;
@@ -8,18 +7,15 @@ import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
-import com.nostra13.universalimageloader.core.ImageLoader;
 import com.rat.networkmanager.R;
 import com.rat.nm.activity.base.BaseActivity;
-
 import com.rat.nm.view.MenuItemView;
 
 public class MenuActivity extends BaseActivity {
-
     @ViewInject(R.id.top_name)
-    private TextView titleView;
+    private TextView topTitleView;
     @ViewInject(R.id.top_left)
-    private TextView backView;
+    private TextView topLeftView;
 
     @ViewInject(R.id.profileMIV)
     private MenuItemView profileMIV;
@@ -50,9 +46,9 @@ public class MenuActivity extends BaseActivity {
      * 初始化界面
      */
     public void initView() {
-        titleView.setText(R.string.menu);
-        backView.setBackgroundResource(R.mipmap.arrow_back);
-        backView.setOnClickListener(this);
+        topTitleView.setText(R.string.menu);
+        topLeftView.setBackgroundResource(R.mipmap.arrow_back);
+        topLeftView.setOnClickListener(this);
 
         profileMIV.initView(MenuItemView.PROFILE);
         profileMIV.initData(0);
