21c5b33c45b8189e786e35320b92f12e0608bf90
laoshubuluo <541276229@qq.com>
2016-09-18 17:36:05 +0800

    更新UI，增加夜间模式

--- app/src/main/java/com/rat/snowkids/activity/MainActivity.java
+++ app/src/main/java/com/rat/snowkids/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.rat.snowkids.activity.base.BaseActivity;
 import com.rat.snowkids.activity.base.WebActivity;
@@ -31,8 +30,8 @@ public class MainActivity extends BaseActivity {
     private TextView topLeftView;
     private TextView topRightView;
     private RelativeLayout topLayout;
-    private LinearLayout mainLL;
-
+    private LinearLayout remindLL;
+    private LinearLayout powerDataLL;
     private RelativeLayout powerFullRemindRL;
     private RelativeLayout theftProofRemindRL;
     private ImageView powerFullRemindIV;
@@ -67,7 +66,8 @@ public class MainActivity extends BaseActivity {
         topLeftView = (TextView) findViewById(R.id.top_left);
         topRightView = (TextView) findViewById(R.id.top_right);
         topLayout = (RelativeLayout) findViewById(R.id.top_layout);
-        mainLL = (LinearLayout) findViewById(R.id.mainLL);
+        powerDataLL = (LinearLayout) findViewById(R.id.powerDataLL);
+        remindLL = (LinearLayout) findViewById(R.id.remindLL);
         powerFullRemindRL = (RelativeLayout) findViewById(R.id.powerFullRemindRL);
         theftProofRemindRL = (RelativeLayout) findViewById(R.id.theftProofRemindRL);
         powerFullRemindIV = (ImageView) findViewById(R.id.powerFullRemindIV);
@@ -95,28 +95,15 @@ public class MainActivity extends BaseActivity {
      */
     public void initData() {
         Power power = PowerUtil.getPowerData(getApplicationContext());
-        // 夜间模式
+        // 夜晚模式
         if (AppUtils.getInstance().isNightMode() && DateUtil.isOnNight()) {
-            powerStatusTV1.setText(getString(R.string.night_model_time_setting));
-            powerStatusTV2.setText(getString(R.string.night_model_from_10_to_7));
-
-            mainLL.setBackgroundColor(getResources().getColor(R.color.gray_3b));
-            topLayout.setBackgroundColor(getResources().getColor(R.color.gray_61));
-            powerFullRemindRL.setBackgroundColor(getResources().getColor(R.color.gray_61));
-            theftProofRemindRL.setBackgroundColor(getResources().getColor(R.color.gray_61));
-            marketLL.setBackgroundColor(getResources().getColor(R.color.gray_52));
-            marketJDIV.setImageResource(R.mipmap.jingdong_logo_night);
-            marketTBIV.setImageResource(R.mipmap.taobao_logo_night);
-            marketLine.setBackgroundColor(getResources().getColor(R.color.gray_8d));
+            initData4Night();
         }
         // 白天模式
         else {
-            powerStatusTV1.setText(getString(R.string.power_time_out));
-            powerStatusTV2.setText(power.getTimeLeft());
+            initData4Day(power);
         }
 
-        // TODO by L.jinzhu for。。。
-//        customView.re
         powerPercentTV.setText(power.getBatteryPct());
         if (power.isCharging())
             powerStatusTV.setText(getApplication().getString(R.string.power_ing));
@@ -127,6 +114,40 @@ public class MainActivity extends BaseActivity {
 
         initRemindStatus4PowerFull();
         initRemindStatus4TheftProof();
+    }
+
+    /**
+     * 初始化数据-白天
+     */
+    public void initData4Day(Power power) {
+        // 内容不同
+        powerStatusTV1.setText(getString(R.string.power_time_out));
+        powerStatusTV2.setText(power.getTimeLeft());
+        // 背景色不同
+        topLayout.setBackgroundColor(getResources().getColor(R.color.white_f8));
+        powerDataLL.setBackgroundColor(getResources().getColor(R.color.white_f2));
+        remindLL.setBackgroundColor(getResources().getColor(R.color.white_ed));
+        marketLL.setBackgroundColor(getResources().getColor(R.color.gray_c3));
+        marketJDIV.setImageResource(R.mipmap.jingdong_logo);
+        marketTBIV.setImageResource(R.mipmap.taobao_logo);
+        marketLine.setBackgroundColor(getResources().getColor(R.color.gray_8f));
+    }
+
+    /**
+     * 初始化数据-夜晚
+     */
+    public void initData4Night() {
+        // 内容不同
+        powerStatusTV1.setText(getString(R.string.night_model_time_setting));
+        powerStatusTV2.setText(getString(R.string.night_model_from_10_to_7));
+        // 背景色不同
+        topLayout.setBackgroundColor(getResources().getColor(R.color.gray_61));
+        powerDataLL.setBackgroundColor(getResources().getColor(R.color.gray_3b));
+        remindLL.setBackgroundColor(getResources().getColor(R.color.gray_46));
+        marketLL.setBackgroundColor(getResources().getColor(R.color.gray_52));
+        marketJDIV.setImageResource(R.mipmap.jingdong_logo_night);
+        marketTBIV.setImageResource(R.mipmap.taobao_logo_night);
+        marketLine.setBackgroundColor(getResources().getColor(R.color.gray_8f));
     }
 
     /**
@@ -230,6 +251,7 @@ public class MainActivity extends BaseActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             LogUtil.i("receive broadcast,action:" + intent.getAction());
+            Power power = PowerUtil.getPowerData(getApplicationContext());
             // 满电提醒
             if (Actions.POWER_FULL_REMIND_STATUS_CHANGE.equals(intent.getAction())) {
                 initRemindStatus4PowerFull();
@@ -242,13 +264,15 @@ public class MainActivity extends BaseActivity {
             else if (Actions.NIGHT_MODEL_STATUS_CHANGE.equals(intent.getAction())) {
                 // 当前是夜间 && 非夜间模式
                 if (DateUtil.isOnNight() && !AppUtils.getInstance().isNightMode()) {
-                    // TODO by L.jinzhu for 取消夜间模式
-                    Toast.makeText(getApplicationContext(), "取消夜间模式功能，待开发", Toast.LENGTH_SHORT).show();
+                    initData4Day(power);
+                } else {
+                    initData4Night();
                 }
+                initRemindStatus4PowerFull();
+                initRemindStatus4TheftProof();
             }
             // 连接状态变更
             else if (Actions.POWER_CONNECT_STATUS_CHANGE.equals(intent.getAction())) {
-                Power power = PowerUtil.getPowerData(getApplicationContext());
                 // 开始充电
                 if (power.isCharging()) {
                     powerStatusTV.setText(getApplication().getString(R.string.power_ing));
